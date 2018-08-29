9eadcb90162003a6d2a1e7d49d38424e4354ae49
Peter Malina <peter.malina@flowup.cz>
2018-08-29 17:24:47 +0200

    tenant: serve can now be called directly from tenants to start server on a default port

--- tenant.go
+++ tenant.go
@@ -1,1 +1,26 @@
 package opentransaction
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+func Serve(server TenantServer) error {
+	lis, err := net.Listen("tcp", "50051")
+	if err != nil {
+		return err
+	}
+
+	s := grpc.NewServer()
+	RegisterTenantServer(s, server)
+	reflection.Register(s)
+
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return err
+}
